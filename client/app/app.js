79191ae4c0df50dc4e433b2e59c2250274773a68
Yann Carlen <yann.carlen@epitech.eu>
2017-04-13 14:37:24 +0200

    bootstrap integration

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,21 @@
+import 'bootstrap-loader'
+const $ = require('jquery')
+window.jQuery = $
+
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import uiBootstrap from 'angular-ui-bootstrap';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
-    uiRouter,
-    Common,
-    Components
-  ])
+  uiRouter,
+  uiBootstrap,
+  Common,
+  Components
+])
   .config(($locationProvider) => {
     "ngInject";
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
