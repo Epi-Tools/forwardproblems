dbbeb9afd245f899e019a53c23e5a80d14a59fbf
Yann Carlen <yann.carlen@epitech.eu>
2017-04-14 00:01:47 +0200

    make unit test ok

--- client/app/common/authLogin/authLogin.spec.js
+++ client/app/common/authLogin/authLogin.spec.js
@@ -10,7 +10,7 @@ describe('AuthLogin', () => {
   beforeEach(inject((_$rootScope_) => {
     $rootScope = _$rootScope_;
     makeController = () => {
-      return new AuthLoginController();
+      return new AuthLoginController(null, $rootScope);
     };
   }));
 
@@ -20,10 +20,10 @@ describe('AuthLogin', () => {
 
   describe('Controller', () => {
     // controller specs
-    /*it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
+    it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
       let controller = makeController();
       expect(controller).to.have.property('name');
-    });*/
+    });
   });
 
   describe('Template', () => {
