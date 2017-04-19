3aaa58016f6bbb4ccbaaa4fd3d6436cc002ab0da
Quentin Muller <quent1.mullerpro@gmail.com>
2017-04-19 17:17:44 +0200

    add footer design

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import Navbar from './navbar/navbar';
 import User from './user/user';
 import authLogin from './authLogin/authLogin';
+import Footer from './footer/footer';
 
 let commonModule = angular.module('app.common', [
   Navbar,
   User,
-  authLogin
+  authLogin,
+  Footer
 ])
 
 .name;
