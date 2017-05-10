cc2aa8591ce3d426cc97f727dfc52991ff8f1295
Quentin Muller <quent1.mullerpro@gmail.com>
2017-05-10 12:05:19 +0200

    Fix merge

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
-import angular from 'angular';
-import Home from './home/home';
-import Comments from './comments/comments';
-import Pools from './pools/pools';
-import Problems from './problems/problems';
-=======
 import angular from 'angular'
 import Home from './home/home'
 import Comments from './comments/comments'
 import Pools from './pools/pools'
 import Messages from './messages/messages'
->>>>>>> 26a1730b472a50a7543e4f0bb4d9f9dcf32ba96c
+import Problems from './problems/problems';
 
 const componentModule = angular.module('app.components', [
   Home,
   Comments,
   Pools,
-<<<<<<< HEAD
-  Problems
-=======
+  Problems,
   Messages
->>>>>>> 26a1730b472a50a7543e4f0bb4d9f9dcf32ba96c
 ])
 
 .name
